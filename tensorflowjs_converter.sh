9847d487f79b378ce1b791c8c513c782c8778642
Emanuele Sabellico <emasab@gmail.com>
2019-12-17 20:11:43 +0100

    quantization for mobile

--- tensorflowjs_converter.sh
+++ tensorflowjs_converter.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 version=v10
-tensorflowjs_converter  --quantization_bytes=2 --input_format=tf_saved_model --output_format=tfjs_graph_model --signature_name=serving_default out/$version connect4-web/$version
+tensorflowjs_converter  --input_format=tf_saved_model --output_format=tfjs_graph_model --signature_name=serving_default --quantization_bytes=2 out/$version connect4-web/$version.quant
